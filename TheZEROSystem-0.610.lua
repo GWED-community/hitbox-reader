27ade4a5a1b7915a874f96961e04f1e6c6e5928f
Tam Nguyen <softwaredeveloptam@gmail.com>
2021-03-31 19:16:07 +0900

    Add new Lua File and fix README
    > Co-authored-by: ComboKyo <ComboKyo@users.noreply.github.com>
    > Co-authored-by: Survivor <https://twitter.com/endless_duel>

---
+++ TheZEROSystem-0.610.lua
@@ -0,0 +1,709 @@
+
+gui.defaultPixelFont(1)
+resetgame = 0
+healthlock = -1
+menuselect = 1
+option = {}
+option[1] = -1
+option[2] = -1
+option[3] = -1
+option[4] = -1
+option[5] = -1
+option[6] = -1
+option[7] = -1
+option[8] = -1
+option[9] = -1
+memory.usememorydomain("APURAM")
+volume = 255
+memory.usememorydomain("WRAM")
+recenthit = {}
+recenthit[1] = 0
+recenthit[2] = 0
+combop = {}
+combop[1] = mainmemory.read_u8(0x1BA8)
+combop[2] = mainmemory.read_u8(0x1BA8)
+firstframeofhitp = {}
+firstframeofhitp[1] = 0
+firstframeofhitp[2] = 0
+health = {}
+health[1] = mainmemory.read_s16_le(0x1B70)
+health[2] = mainmemory.read_s16_le(0x1B74)
+totaldamage = {}
+totaldamage[1] = 0
+totaldamage[2] = 0
+stageselectlabels = {}
+stageselectlabels[0] = "Default"
+stageselectlabels[1] = "Heavyarms"
+stageselectlabels[2] = "Deathscythe"
+stageselectlabels[3] = "Shenlong"
+stageselectlabels[4] = "Wing/Wing.0"
+stageselectlabels[5] = "Sandrock"
+stageselectlabels[6] = "Vayaete"
+stageselectlabels[7] = "Tallgeese"
+stageselectlabels[8] = "Epyon"
+stageselectlabels[9] = "Mercurius"
+stageselectlabels[10] = "???"
+stageselectlabels[11] = "????"
+
+
+function globals()
+	camx = mainmemory.read_s16_le(0x0620)
+	camy = mainmemory.read_s16_le(0x0622)
+	p1posx = mainmemory.read_s16_le(0x1101)
+	p1posy = mainmemory.read_s16_le(0x1181)
+	p2posx = mainmemory.read_s16_le(0x1105)
+	p2posy = mainmemory.read_s16_le(0x1185)
+	p1statetype = mainmemory.read_u8(0x1200)
+	p2statetype = mainmemory.read_u8(0x1204)
+
+	firstframeofhitp[1] = ((firstframeofhitp[1] + 1) * ((mainmemory.read_u8(0x1BA8)>0 and mainmemory.read_u8(0x1BA8)>combop[1]) and 1 or 0))-- + (mainmemory.read_u8(0x1BA8)>combop[1] and 0 or 0)
+	firstframeofhitp[2] = ((firstframeofhitp[2] + 1) * ((mainmemory.read_u8(0x1BAC)>0 and mainmemory.read_u8(0x1BAC)>combop[2]) and 1 or 0))-- + (mainmemory.read_u8(0x1BAC)>combop[2] and 0 or 0)
+
+end
+function lateglobals()
+	health[1] = mainmemory.read_s16_le(0x1B70)
+	health[2] = mainmemory.read_s16_le(0x1B74)
+	combop[1] = mainmemory.read_u8(0x1BA8)
+	combop[2] = mainmemory.read_u8(0x1BAC)
+
+end
+
+function inputpress()
+	buttons = joypad.get()
+	if buttons["P1 Up"] == true then
+		buttonp1up = buttonp1up + 1
+	else
+		buttonp1up	= 0
+	end
+	if buttons["P1 Down"] == true then
+		buttonp1down = buttonp1down + 1
+	else
+		buttonp1down 		= 0
+	end
+	if buttons["P1 Left"] == true then
+		buttonp1left = buttonp1left + 1
+	else
+		buttonp1left		= 0
+	end
+	if buttons["P1 Right"] == true then
+		buttonp1right = buttonp1right + 1
+	else
+		buttonp1right 		= 0
+	end
+	if buttons["P1 A"] == true then
+		buttonp1a = buttonp1a + 1
+	else
+		buttonp1a 		= 0
+	end
+	if buttons["P1 Y"] == true then
+		buttonp1y = buttonp1y + 1
+	else
+		buttonp1y 		= 0
+	end
+	if buttons["P1 B"] == true then
+		buttonp1b = buttonp1b + 1
+	else
+		buttonp1b 		= 0
+	end
+	if buttons["P1 L"] == true then
+		buttonp1l = buttonp1l + 1
+	else
+		buttonp1l 		= 0
+	end
+	if buttons["P1 R"] == true then
+		buttonp1r = buttonp1r + 1
+	else
+		buttonp1r 		= 0
+	end
+	if buttons["P1 Select"] == true then
+		buttonp1select = buttonp1select + 1
+	else
+		buttonp1select 		= 0
+	end
+	if buttons["P1 Start"] == true then
+		buttonp1start = buttonp1start + 1
+	else
+		buttonp1start 		= 0
+	end
+	if buttons["P2 Up"] == true then
+		buttonp2up = buttonp2up + 1
+	else
+		buttonp2up	= 0
+	end
+	if buttons["P2 Down"] == true then
+		buttonp2down = buttonp2down + 1
+	else
+		buttonp2down 		= 0
+	end
+	if buttons["P2 Left"] == true then
+		buttonp2left = buttonp2left + 1
+	else
+		buttonp2left		= 0
+	end
+	if buttons["P2 Right"] == true then
+		buttonp2right = buttonp2right + 1
+	else
+		buttonp2right 		= 0
+	end
+	if buttons["P2 A"] == true then
+		buttonp2a = buttonp2a + 1
+	else
+		buttonp2a 		= 0
+	end
+	if buttons["P2 Y"] == true then
+		buttonp2y = buttonp2y + 1
+	else
+		buttonp2y 		= 0
+	end
+	if buttons["P2 B"] == true then
+		buttonp2b = buttonp2b + 1
+	else
+		buttonp2b 		= 0
+	end
+	if buttons["P2 L"] == true then
+		buttonp2l = buttonp2l + 1
+	else
+		buttonp2l 		= 0
+	end
+	if buttons["P2 R"] == true then
+		buttonp2r = buttonp2r + 1
+	else
+		buttonp2r 		= 0
+	end
+	if buttons["P2 Select"] == true then
+		buttonp2select = buttonp2select + 1
+	else
+		buttonp2select 		= 0
+	end
+	if buttons["P2 Start"] == true then
+		buttonp2start = buttonp2start + 1
+	else
+		buttonp2start 		= 0
+	end
+
+end
+
+local function reset()
+	if buttons["P1 L"] == true and buttons["P1 R"] == true and buttons["P1 Start"] == true and buttons["P1 Select"] == true then
+		resetgame = resetgame + 1
+	else
+		resetgame = 0
+	end
+
+	if resetgame > 119 then
+		mainmemory.write_u8(0x00, 0) 
+		resetgame = 0
+	end
+
+end
+
+local function hpdisplay()
+	local HP1 = (mainmemory.read_s16_le(0x1B70) / 16)
+	local HP2 = (mainmemory.read_s16_le(0x1B74) / 16)
+	
+	if pause < 0 then
+		hpcolor = 0xFF40FFFF
+	else 
+		hpcolor = 0xFF4080FF	
+	end
+	
+	gui.pixelText(  15, 32, math.floor(HP1) .. "/1807", hpcolor, 0x00000000) 
+	gui.pixelText( 143, 32, math.floor(HP2) .. "/1807", hpcolor, 0x00000000) 
+
+--if bit.band(p1statetype, 128) == 128 then
+--	gui.pixelText(  15, 4, "Cancel Ready!", 0xFFFFFF00, 0x00000000) 
+--end
+
+--if bit.band(p2statetype, 128) == 128 then
+--	gui.pixelText( 143, 4, "Cancel Ready!", 0xFFFFFF00, 0x00000000) 
+--end
+
+end
+
+local function player()
+
+	local pfacing = 1
+
+	for i = 0,1, 1 do
+		local pstart 	= 0x1100 + (0x04 * i)
+		local pposx    	= mainmemory.read_s16_le(pstart + 0x01)
+		local pposy    	= mainmemory.read_s16_le(pstart + 0x80 + 0x01)
+		local plengthx 	= mainmemory.read_s16_le(pstart + 0x700 + 0x02)
+		local plengthy 	= mainmemory.read_s16_le(pstart + 0x700 + 0x80 + 0x02)			
+		local poffsetx	= mainmemory.read_s16_le(pstart + 0x700)
+		local poffsety	= mainmemory.read_s16_le(pstart + 0x700 + 0x80)
+		local pface     = mainmemory.read_u8	(pstart + 0x200 + 0x80 + 0x03)
+		local pmovetype = mainmemory.read_u8	(pstart + 0x100)
+		local pstate 	= mainmemory.read_u8	(pstart + 0x101)
+		local pattack	= mainmemory.read_u8	(pstart + 0x500) 
+		local pnograb 	= mainmemory.read_u8	(0x1BE2 + (i * 0x04))
+		
+		if  bit.band( pface, 64) == 64 then
+			pfacing = -1
+		else
+			pfacing = 1
+		end
+
+		local boxborder
+		local boxfill
+
+		if  bit.band( pstate, 16) == 16 then
+			boxborder 	= 0xFFFF8888
+			boxfill 	= 0x40CC6666
+		elseif  bit.band( pstate, 2) == 2 then
+			boxborder	= 0xFF00FFFF
+			boxfill 	= 0x4000FFFF
+		elseif  bit.band( pmovetype, 8) == 8 then
+			boxborder	= 0xFF000000
+			boxfill 	= 0x60000000
+		elseif  pnograb > 0 then
+			boxborder	= 0xFF0080FF
+			boxfill 	= 0x400080FF
+		else
+			boxborder	= 0xFF0000FF
+			boxfill  	= 0x400000FF
+		end
+
+		gui.drawBox(pposx - camx + (poffsetx * pfacing ),
+					pposy - camy + (poffsety),
+					pposx - camx + ( (poffsetx + plengthx) * pfacing ),
+					pposy - camy + (poffsety + plengthy),
+					boxborder, boxfill)
+
+	end
+end
+
+local function recovery()
+
+	local p1recovery = mainmemory.read_u8(0x1b1A)
+		if p1recovery > 0 then
+			gui.pixelText(	p1posx - camx - 8,
+							p1posy - camy - 8,
+							mainmemory.read_u8(0x1B1A),
+							0xFFFFFFFF, 0xB0000000) 
+		end
+
+	local p2recovery = mainmemory.read_u8(0x1b1E)
+		if p2recovery > 0 then
+			gui.pixelText(	p2posx - camx - 8,
+							p2posy - camy - 8,
+							mainmemory.read_u8(0x1B1E),
+							0xFFFFFFFF, 0xB0000000) 
+		end
+		
+end
+local function magicbox()
+
+local pfacing = 1
+
+	for i = 0,1, 1 do
+		local pstart 	= 0x1100 + (0x04 * i)
+		local pposx    	= mainmemory.read_s16_le(pstart + 0x01)
+		local pposy    	= mainmemory.read_s16_le(pstart + 0x80 + 0x01)
+		local poffsetx	= mainmemory.read_s16_le(pstart + 0xAC0)
+		local poffsety	= mainmemory.read_s16_le(pstart + 0xAC0 + 0x02)
+		local plengthx 	= mainmemory.read_s16_le(pstart + 0x600 + 0x80 + 0x02)
+		local plengthy 	= mainmemory.read_s16_le(pstart + 0xAC0 + 0x08)			
+		local pface     = mainmemory.read_u8	(pstart + 0x200 + 0x80 + 0x03)
+		local pmovetype = mainmemory.read_u8	(pstart + 0x100)
+		local pstate 	= mainmemory.read_u8	(pstart + 0x101)
+		local pattack	= mainmemory.read_u8	(pstart + 0x500) 
+		
+		if  bit.band( pface, 64) == 64 then
+			pfacing = -1
+		else
+			pfacing = 1
+		end
+
+		local boxborder
+		local boxfill
+
+		if  bit.band( pstate, 16) == 16 then
+			boxborder 	= 0xFFFF8888
+			boxfill 	= 0x40CC6666
+		elseif  bit.band( pstate, 2) == 2 then
+			boxborder	= 0xFF00FFFF
+			boxfill 	= 0x4000FFFF
+		elseif  bit.band( pmovetype , 8) == 8 then
+			boxborder	= 0xFF000000
+			boxfill 	= 0x60000000
+		else
+			boxborder	= 0xFF0000FF
+			boxfill  	= 0x400000FF
+		end
+
+	if plengthx > -1 then
+		gui.drawBox(pposx - camx + (poffsetx * pfacing ),
+					pposy - camy + (poffsety),
+					pposx - camx + ( (poffsetx + plengthx) * pfacing ),
+					pposy - camy + (poffsety + plengthy),
+					boxborder, boxfill)
+	end
+	end
+end
+
+local function redboxes()
+
+	for i = 0,1, 1 do
+		local pstart 	= 0x1100 + (0x04 * i)
+		local pposx    	= mainmemory.read_s16_le(pstart + 0x01)
+		local pposy    	= mainmemory.read_s16_le(pstart + 0x80 + 0x01)
+		local plengthx 	= mainmemory.read_s16_le(pstart + 0x800 + 0x02)
+		local plengthy 	= mainmemory.read_s16_le(pstart + 0x800 + 0x80 + 0x02)			
+		local poffsetx	= mainmemory.read_s16_le(pstart + 0x800)
+		local poffsety	= mainmemory.read_s16_le(pstart + 0x800 + 0x80)
+		local pmovetype = mainmemory.read_u8	(pstart + 0x100)
+		local pstate 	= mainmemory.read_u8	(pstart + 0x101)
+		local pattack	= mainmemory.read_u8	(pstart + 0x500) 
+		local pface     = mainmemory.read_u8	(pstart + 0x200 + 0x80 + 0x03)
+
+		if  bit.band(pface, 64) == 64 then
+			pfacing = -1
+		else
+			pfacing = 1
+		end
+
+		local boxborder	= 0xFFFF0000
+		local boxfill	= 0x40FF0000
+
+		if pattack > 0 then
+		gui.drawBox(pposx - camx + (poffsetx * pfacing),
+					pposy - camy + (poffsety),
+					pposx - camx + ( (poffsetx + plengthx) * pfacing ),
+					pposy - camy + (poffsety + plengthy),
+					boxborder, boxfill)
+		end
+	end
+end
+
+local function scaler()
+    xs = client.screenwidth() / 256
+    ys = client.screenwidth() / 224
+end
+
+local function projectiles()
+
+	local pjstart
+
+	for i = 0,7, 1 do
+
+		pjstart = 0x1140 + (0x04 * i)
+		local pjposx    = mainmemory.read_s16_le(pjstart + 0x01)
+		local pjposy    = mainmemory.read_s16_le(pjstart + 0x80 + 0x01)
+		local pjlengthx = mainmemory.read_s16_le(pjstart + 0x800 + 0x02)
+		local pjlengthy = mainmemory.read_s16_le(pjstart + 0x800 + 0x80 + 0x02)			
+		local pjoffsetx = mainmemory.read_s16_le(pjstart + 0x800)
+		local pjoffsety = mainmemory.read_s16_le(pjstart + 0x800 + 0x80)
+
+		local pjface    = mainmemory.read_u8	(pjstart + 0x200 + 0x80 + 0x03)
+		local pjenable  = mainmemory.read_s16_le(pjstart - 0x100)
+		local facing = 1
+
+		if  bit.band( pjface, 64) == 64 then
+			facing = -1
+		else
+			facing = 1
+		end
+
+		if pjenable > 0 then
+		gui.drawBox(pjposx - camx + (pjoffsetx * facing),
+					pjposy - camy + (pjoffsety),
+					pjposx - camx + ( (pjoffsetx + pjlengthx) * facing ),
+					pjposy - camy + (pjoffsety + pjlengthy),
+					0xFFFF0000,
+					0x40000000)
+		end
+	end
+end
+
+local function vulcans()
+
+	local pjstart
+
+	for i = 0,7, 1 do
+
+		pjstart = 0x1120 + (0x04 * i)
+		local pjposx    = mainmemory.read_s16_le(pjstart + 0x01)
+		local pjposy    = mainmemory.read_s16_le(pjstart + 0x80 + 0x01)
+		local pjlengthx = mainmemory.read_s16_le(pjstart + 0x800 + 0x02)
+		local pjlengthy = mainmemory.read_s16_le(pjstart + 0x800 + 0x80 + 0x02)			
+		local pjoffsetx = mainmemory.read_s16_le(pjstart + 0x800)
+		local pjoffsety = mainmemory.read_s16_le(pjstart + 0x800 + 0x80)
+
+		local pjface    = mainmemory.read_u8	(pjstart + 0x200 + 0x80 + 0x03)
+		local pjenable  = mainmemory.read_s16_le(pjstart - 0x100)
+		local facing = 1
+
+		if  bit.band( pjface, 64) == 64 then
+			facing = -1
+		else
+			facing = 1
+		end
+
+		if pjenable > 0 then
+		gui.drawBox(pjposx - camx + (pjoffsetx * facing),
+					pjposy - camy + (pjoffsety),
+					pjposx - camx + ( (pjoffsetx + pjlengthx) * facing  ),
+					pjposy - camy + (pjoffsety + pjlengthy),
+					0xFFFF0000,
+					0x40000000)
+		end
+	end
+end
+
+local function pausemenu()
+	
+	local menulength = 9
+	local menuoffsetx = -16
+	local menuoffsety = 24 + (-menulength * 4)
+	
+	if buttonp1down == 1 then
+		menuselect = menuselect + 1
+		if menuselect > menulength then
+			menuselect = 1
+		end
+	elseif buttonp1up == 1 then
+		menuselect = menuselect - 1
+		if menuselect < 1 then
+			menuselect = menulength
+		end
+	end
+
+	if buttonp2down == 1 then
+		menuselect = menuselect + 1
+		if menuselect > menulength then
+			menuselect = 1
+		end
+	elseif buttonp2up == 1 then
+		menuselect = menuselect - 1
+		if menuselect < 1 then
+			menuselect = menulength
+		end
+	end
+
+	local pausemenulabels = {}
+	pausemenulabels[1] = "Max HP"
+	pausemenulabels[2] = "Max Super Meter"
+	pausemenulabels[3] = "Disable Timer"
+	pausemenulabels[4] = "Hide Hitboxes"
+	pausemenulabels[5] = "Hide HP values"
+	pausemenulabels[6] = "Disable Lifebar Display"
+	pausemenulabels[7] = "Disable Vulcans"
+	pausemenulabels[8] = "Volume"
+	pausemenulabels[9] = "Hide Damage"
+	
+	
+	if menuselect == 8 then
+		memory.usememorydomain("APURAM")
+		if volume < 255 and (buttonp1right > 0 or buttonp2right > 0) then
+			volume = volume + 1
+			memory.write_u8(0x0C, volume)
+		elseif volume > 0 and (buttonp1left > 0 or buttonp2left > 0) then
+			volume = volume - 1
+			memory.write_u8(0x0C, volume)
+		end
+		gui.pixelText( menuoffsetx + 94, menuoffsety + 80 + (8*8), volume, 0xFFFF0000, 0x40000000)
+		memory.usememorydomain("WRAM")
+	end
+
+	if buttonp1a == 1 then
+		option[menuselect] = option[menuselect]	* -1
+	end
+	if buttonp2a == 1 then
+		option[menuselect] = option[menuselect]	* -1
+	end
+	
+	for i = 1,menulength, 1 do
+		if menuselect == i then
+			textcolor 	= 0xFF00AAFF
+			textback	= 0x00000000
+		else
+			textcolor 	= 0xFFFFFFFF
+			textback	= 0x00000000			
+		end
+		gui.pixelText( menuoffsetx + 112, menuoffsety + 80 + (i*8), pausemenulabels[i], textcolor, textback)
+		if option[i] == 1 then
+			gui.pixelText( menuoffsetx + 104, menuoffsety + 80 + (i*8), "O", i~=8 and 0xFFFF0000 or 0x00000000, 0x00000000)
+		end
+		
+	end
+end
+
+local function settings()
+	if option[1] == 1 then
+		mainmemory.write_s16_le(0x1B70, 1807*16)
+		mainmemory.write_s16_le(0x1B74, 1807*16)
+	end
+	if option[2] == 1 then
+		mainmemory.write_s16_le(0x1B80, 300)
+		mainmemory.write_s16_le(0x1B84, 300)		
+	end	
+--	if option[3] == 1 then
+--		memory.usememorydomain("APURAM")
+--		mainmemory.write_u8(0x000C, 0)
+--		memory.usememorydomain("WRAM")
+--	elseif option[3] == -1 then
+--		memory.usememorydomain("APURAM")
+--		if pause < 0 then
+--			mainmemory.write_u8(0x000C, 128)
+--		else
+--			mainmemory.write_u8(0x000C, 255)
+--		end
+--		memory.usememorydomain("WRAM")	
+--	end
+	if option[3] == 1 then 
+		mainmemory.write_u8(0xFF02, 2)
+	elseif option[3] == -1 then
+		mainmemory.write_u8(0xFF02, 0)	
+	end
+
+--	if option[4] == 1 then
+--		
+--	end
+end
+
+local function stageselectversus()
+
+	local menulength = 12
+	local menuselect = ( mainmemory.read_u8(0x00060E) * 0.5)
+	if buttonp1up == 1 then
+		menuselect = menuselect + 1
+		if menuselect > (menulength - 1) then
+			menuselect = 0
+		end
+	elseif buttonp1down == 1 then
+		menuselect = menuselect - 1
+		if menuselect < 0 then
+			menuselect = menulength - 1
+		end
+	end
+
+	if buttonp2up == 1 then
+		menuselect = menuselect + 1
+		if menuselect > menulength then
+			menuselect = 1
+		end
+	elseif buttonp2down == 1 then
+		menuselect = menuselect - 1
+		if menuselect < 1 then
+			menuselect = menulength
+		end
+	end
+	if true then
+		gui.pixelText( 70, 8, "Stage Select:", 0xFFFFFFFF, 0x00000000)
+		gui.pixelText( 124, 8, stageselectlabels[menuselect], 0xFFFFFFFF, 0x00000000)
+	end
+	mainmemory.write_u8(0x00060E, (menuselect * 2))
+	
+	if menuselect == 0 then
+		mainmemory.write_u8(0x000038, 10)
+	end
+end
+
+local function lifebars()
+
+	local HP1 = (mainmemory.read_s16_le(0x1B70) / 16)
+	local HP2 = (mainmemory.read_s16_le(0x1B74) / 16)
+
+	gui.drawImage("images/Lifebars.png", 4, 2, 100 * (HP1/1807)) 
+
+end
+
+local function displaydamage()
+
+-- p2 is first
+	for i = 0,1, 1 do
+		local offset = 0x1B70 + (i * (0x04))
+		if firstframeofhitp[2-i] == 1 then --and combop[i+1] < mainmemory.read_u8(offset + 0x38) then --		if mainmemory.read_s16_le(offset) < health[i] then
+			recenthit[i] = math.floor((health[i+1] - mainmemory.read_s16_le(offset))) / 16
+			if mainmemory.read_u8(0x1BAC - (i * 0x04)) == 1 then --offset + 0x38
+				totaldamage[i+1] = 0
+			end
+			totaldamage[i+1] = totaldamage[i+1] + recenthit[i]
+		end
+		gui.pixelText(  29 + (i * 140),	 0, "Damage:", 0xFFFFFFFF, 0x00000000)
+		gui.pixelText( 	35 + 22 + (i * 140),  0, recenthit[i], 0xFFFF0000, 0x00000000)
+	
+		gui.pixelText(   5 + (i * 140),	 8, "Total Damage:", 0xFFFFFFFF, 0x00000000)
+		gui.pixelText( 	35 + 22 + (i * 140),  8, totaldamage[i+1], 0xFFFF0000, 0x00000000)
+		
+	end
+end
+local function volumeadjust()
+	memory.usememorydomain("APURAM")
+	memory.write_u8(0x0C, (volume * (pause < 0 and .5 or 1)))
+	memory.usememorydomain("WRAM")
+end
+
+function test()
+		gui.pixelText(  29 , 100, "Damage:", 0xFFFFFFFF, 0x00000000)
+end
+
+function main()
+
+	globals()
+	inputpress()
+	gamestate = mainmemory.read_u8(0x000000)
+	matchstate = mainmemory.read_u8(0x000002)
+	pause = mainmemory.read_s16_le(0x000608) -- -1 for pause. 0 for unpause. -2 to pause without darkened screen
+
+--gui.drawImage("images/Portrait09.png", 0, 0, 84, 155) 
+	if gamestate > 0 then
+		reset()
+	end
+	if gamestate == 6 then
+		stageselectversus()
+	elseif gamestate == 8 and matchstate == 32 then
+		gui.pixelText( 140,  8, " ", 0x00000000, 0x00000000)
+		if option[9] == -1 then
+			displaydamage()
+		end
+		----------------------------------------
+		if option[5] == -1 then
+			hpdisplay()
+		end
+		---------------------------------------- Lifebar Disable
+		if option[6] == 1 then
+			mainmemory.write_u8(0x10, 4)
+		elseif option[6] == -1 then
+			mainmemory.write_u8(0x10, 2)
+		end
+		----------------------------------------
+		if option[4] == -1 then
+			player()
+			magicbox()
+			redboxes()
+			projectiles()
+			vulcans()
+			recovery()
+		end	
+		----------------------------------------
+		if pause < 0 then
+			pausemenu()
+		end
+		if option[7] == 1 then
+			mainmemory.write_u8(0x1090, 0)
+			mainmemory.write_u8(0x1094, 0)
+		end
+		settings()
+		----------------------------------------
+	else
+		gui.clearGraphics()
+		gui.cleartext()
+	end
+	--- Add state select music if Stage 00 is selected.
+--	if mainmemory.read_u8(0x00060E) == 0 and gamestate > 5 and gamestate < 9 and matchstate < 32 then
+--		mainmemory.write_u8(0x000038, 10)
+--	end
+	volumeadjust()
+	lateglobals()
+	scaler()
+end
+
+-- Runs all the main code
+event.onframestart( main )
+
+-- Is here just to 
+while true do
+	emu.frameadvance()
+end
